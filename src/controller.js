48195467a940556c5d0cd9ed5c56c0c7a3f9a092
Aviv Ben Ezra <avivbe95@gmail.com>
2025-07-30 13:37:17 +0300

    Make quote div narrower

--- src/controller.js
+++ src/controller.js
@@ -8,43 +8,85 @@ import Renderer from "./view.js";
 
 const renderer = new Renderer();
 
-fetchUsers().then((data) => {
-  const users = data.results;
+let currentPageData = null;
 
-  const mainUser = {
-    firstName: users[0].name.first,
-    lastName: users[0].name.last,
-    picture: users[0].picture.large,
-    city: users[0].location.city,
-    state: users[0].location.state,
-  };
+function generateUserPage() {
+  fetchUsers().then((data) => {
+    const users = data.results;
 
-  const friends = users.slice(1).map((user) => ({
-    firstName: user.name.first,
-    lastName: user.name.last,
-  }));
-
-  renderer.renderMainUser(mainUser);
-  renderer.renderFriends(friends);
-
-  // Fetch and render Kanye quote
-  fetchKanyeQuote().then((data) => {
-    renderer.renderQuote(data.quote);
-  });
-
-  // Fetch and render random Pokemon
-  fetchRandomPokemon().then((data) => {
-    // Convert name to Proper Case
-    const properName = data.name.charAt(0).toUpperCase() + data.name.slice(1).toLowerCase();
-    const pokemon = {
-      name: properName,
-      image: data.sprites.front_default,
+    const mainUser = {
+      firstName: users[0].name.first,
+      lastName: users[0].name.last,
+      picture: users[0].picture.large,
+      city: users[0].location.city,
+      state: users[0].location.state,
     };
-    renderer.renderPokemon(pokemon);
-  });
 
-  // Fetch and render About Me text
-  fetchBaconIpsum().then((data) => {
-    renderer.renderAbout(data[0]);
+    const friends = users.slice(1).map((user) => ({
+      firstName: user.name.first,
+      lastName: user.name.last,
+    }));
+
+    // Fetch all data in parallel
+    Promise.all([fetchKanyeQuote(), fetchRandomPokemon(), fetchBaconIpsum()]).then(
+      ([kanyeData, pokemonData, baconData]) => {
+        const properName =
+          pokemonData.name.charAt(0).toUpperCase() +
+          pokemonData.name.slice(1).toLowerCase();
+        const pokemon = {
+          name: properName,
+          image: pokemonData.sprites.front_default,
+        };
+        const quote = kanyeData.quote;
+        const about = baconData[0];
+
+        // Save current page data for saving
+        currentPageData = {
+          mainUser,
+          friends,
+          quote,
+          pokemon,
+          about,
+        };
+
+        renderer.renderMainUser(mainUser);
+        renderer.renderFriends(friends);
+        renderer.renderQuote(quote);
+        renderer.renderPokemon(pokemon);
+        renderer.renderAbout(about);
+      }
+    );
   });
+}
+
+// Initial page load
+generateUserPage();
+
+// Save User Page
+$(".save-btn").on("click", function () {
+  if (currentPageData) {
+    localStorage.setItem("savedUserPage", JSON.stringify(currentPageData));
+    alert("User page saved!");
+  }
+});
+
+// Load User Page
+$(".load-btn").on("click", function () {
+  const saved = localStorage.getItem("savedUserPage");
+  if (saved) {
+    const data = JSON.parse(saved);
+    currentPageData = data;
+    renderer.renderMainUser(data.mainUser);
+    renderer.renderFriends(data.friends);
+    renderer.renderQuote(data.quote);
+    renderer.renderPokemon(data.pokemon);
+    renderer.renderAbout(data.about);
+  } else {
+    alert("No saved user page found.");
+  }
+});
+
+// Generate new user page
+$(".generate-btn").on("click", function () {
+  generateUserPage();
 });
