4e68b3a145543191555874124000934449a83230
Aviv Ben Ezra <avivbe95@gmail.com>
2025-07-30 14:32:32 +0300

    Add user feedback for errors

--- src/controller.js
+++ src/controller.js
@@ -10,53 +10,75 @@ const renderer = new Renderer();
 
 let currentPageData = null;
 
+function showError(message) {
+  // Remove any existing error
+  $(".error-message").remove();
+  // Show new error at the top of the container
+  $(".container").prepend(
+    `<div class="error-message" style="color:red; margin-bottom:10px;">${message}</div>`
+  );
+  setTimeout(
+    () =>
+      $(".error-message").fadeOut(500, function () {
+        $(this).remove();
+      }),
+    4000
+  );
+}
+
 function generateUserPage() {
-  fetchUsers().then((data) => {
-    const users = data.results;
+  fetchUsers()
+    .then((data) => {
+      const users = data.results;
 
-    const mainUser = {
-      firstName: users[0].name.first,
-      lastName: users[0].name.last,
-      picture: users[0].picture.large,
-      city: users[0].location.city,
-      state: users[0].location.state,
-    };
+      const mainUser = {
+        firstName: users[0].name.first,
+        lastName: users[0].name.last,
+        picture: users[0].picture.large,
+        city: users[0].location.city,
+        state: users[0].location.state,
+      };
 
-    const friends = users.slice(1).map((user) => ({
-      firstName: user.name.first,
-      lastName: user.name.last,
-    }));
+      const friends = users.slice(1).map((user) => ({
+        firstName: user.name.first,
+        lastName: user.name.last,
+      }));
 
-    // Fetch all data in parallel
-    Promise.all([fetchKanyeQuote(), fetchRandomPokemon(), fetchBaconIpsum()]).then(
-      ([kanyeData, pokemonData, baconData]) => {
-        const properName =
-          pokemonData.name.charAt(0).toUpperCase() +
-          pokemonData.name.slice(1).toLowerCase();
-        const pokemon = {
-          name: properName,
-          image: pokemonData.sprites.front_default,
-        };
-        const quote = kanyeData.quote;
-        const about = baconData[0];
+      // Fetch all data in parallel
+      Promise.all([fetchKanyeQuote(), fetchRandomPokemon(), fetchBaconIpsum()])
+        .then(([kanyeData, pokemonData, baconData]) => {
+          const properName =
+            pokemonData.name.charAt(0).toUpperCase() +
+            pokemonData.name.slice(1).toLowerCase();
+          const pokemon = {
+            name: properName,
+            image: pokemonData.sprites.front_default,
+          };
+          const quote = kanyeData.quote;
+          const about = baconData[0];
 
-        // Save current page data for saving
-        currentPageData = {
-          mainUser,
-          friends,
-          quote,
-          pokemon,
-          about,
-        };
+          // Save current page data for saving
+          currentPageData = {
+            mainUser,
+            friends,
+            quote,
+            pokemon,
+            about,
+          };
 
-        renderer.renderMainUser(mainUser);
-        renderer.renderFriends(friends);
-        renderer.renderQuote(quote);
-        renderer.renderPokemon(pokemon);
-        renderer.renderAbout(about);
-      }
-    );
-  });
+          renderer.renderMainUser(mainUser);
+          renderer.renderFriends(friends);
+          renderer.renderQuote(quote);
+          renderer.renderPokemon(pokemon);
+          renderer.renderAbout(about);
+        })
+        .catch(() =>
+          showError(
+            "Failed to fetch data from one or more APIs. Please try again."
+          )
+        );
+    })
+    .catch(() => showError("Failed to fetch users. Please try again."));
 }
 
 // Initial page load
@@ -79,7 +101,9 @@ $(".save-btn").on("click", function () {
     let usersObj = JSON.parse(localStorage.getItem("savedUserPages") || "{}");
     const username = `${currentPageData.mainUser.firstName} ${currentPageData.mainUser.lastName}`;
     if (usersObj[username]) {
-      alert("A user with this name already exists. Please generate a new user or use a unique name.");
+      alert(
+        "A user with this name already exists. Please generate a new user or use a unique name."
+      );
       return;
     }
     usersObj[username] = currentPageData;
